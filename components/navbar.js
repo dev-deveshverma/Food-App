4747bdd664cfbcd76737daf6bdbfde335a492390
DEVESH VERMA <71995793+webdeveshverma@users.noreply.github.com>
2022-01-21 23:54:48 +0530

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -3,6 +3,7 @@ function navbar(){
     <div class="navbar">
     <div id="home"><a href="index.html">Home</a></div>
     <div id="Search_Bar"> 
+    
         <input type="text" placeholder="Search Recepie" id="Search_Recepie">
     </div>
     <div id="Latest_Recipes"><a href="latestRecipe.html">Latest Recipes</a></div>
